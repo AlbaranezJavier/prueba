fea345f6644999ebe3248d7ce70a603dc1fd3336
AlbaranezJavier <32954090+AlbaranezJavier@users.noreply.github.com>
2023-01-27 14:11:28 +0100

    up

--- setup.py
+++ setup.py
@@ -5,11 +5,5 @@ setup(
     name="wallace_libs",
     version="2.4.2",
     description="A python module to update the console without flashing.",
-    long_description_content_type="text/markdown",
-    url="https://github.com/Matthias1590/ConsoleDraw",
-    author="Matthias Wijnsma",
-    author_email="matthiasx95@gmail.com",
-    license="MIT",
-    python_requires=">=3.6",
     packages=["wallace_libs"],
 )
